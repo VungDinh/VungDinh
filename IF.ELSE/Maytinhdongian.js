51aba059fef8c5ae1cb752f2a3486bdbcfc71eed
one-piece <one-piece@gmail.com>
2023-07-27 11:13:28 +0700

    add

--- IF.ELSE/Maytinhdongian.js
+++ IF.ELSE/Maytinhdongian.js
@@ -1,8 +1,36 @@
-function caculator(){
-    let a = document.getElementById('a').value;
-    let b = document.getElementById('b').value;
-    let c = document.getElementById('c').value;
-    let delta = b*b-4*a*c;
-    console.log(delta);
-    document.getElementById("result").innerHTML = delta
+// function caculator() {
+//     let inputA = document.getElementById('inputA').value;
+//     let inputB = document.getElementById('inputB').value;
+//     let pheptoan = document.getElementById("pheptoan").value;
+//     switch (caculator) {
+//         case '+':
+//             result = Number(inputA) + Number(inputB);
+//             break;
+//         case '-':
+//             result = Number(inputA) - Number(inputB);
+//             break;
+//         case '*':
+//             result = Number(inputA) * Number(inputB);
+//             break;
+//         case '/':
+//             result = Number(inputA) / Number(inputB);
+//             break;
+//     }
+//     document.getElementById("ketqua").innerHTML = result
+// }
+
+function nhapGiaTri(value){
+    document.getElementById('input').value += value; 
+    // cộng chuỗi
+}
+
+function xoa(){
+    document.getElementById('input').value = '';
+}
+
+function ketqua(){
+    let input = document.getElementById('input').value;
+    let result = eval(input);
+    //  có chức năng tính toán biểu thức toán học 
+    document.getElementById('input').value = result;
 }
